49c6431abb7aa46bd868dfebaef43d0dfec8d242
kimallen <kimmarieallen@gmail.com>
2016-06-06 17:15:25 -0700

    Chain module instead of var app

--- http-server/public/script.js
+++ http-server/public/script.js
@@ -1,8 +1,8 @@
 (function(){
 
-	var app = angular.module('dashboard', ['highcharts-ng']);
-	
-	app.controller("outcomeController", ['$http', outcomeController]);
+	angular
+        .module('dashboard', ['highcharts-ng'])
+        .controller("outcomeController", ['$http', outcomeController]);
     
     function outcomeController($http){
         
@@ -10,9 +10,9 @@
         viewModel.populateCharts = populateCharts
         viewModel.chartConfigs = [];
 
-        viewModel.demoFilter = {
-            demoSelect: "ALL",
-            demoOptions: [
+        viewModel.demogFilter = {
+            demogSelect: "ALL",
+            demogOptions: [
             {value: "ALL", name: "ALL"},
             {value: "age", name: "age"}, 
             {value: "violence", name: "History of violence"},
@@ -28,17 +28,12 @@
         }
 
         var outcomesData = {};
-        //This function retrieves data from api, takes in the filter type, and builds small multiples charts of each of the filter's categories
-        
-        // outcomesDataByFilter("age")
 
-            var apiUrl = 'http://localhost:3000/db'
-            $http.get(apiUrl)
-            .then(storeResponseData)
-            // .then(populateDefaultChart)
+        var apiUrl = 'http://localhost:3000/db'
+        $http.get(apiUrl)
+        .then(storeResponseData)
         
         function storeResponseData(response){
-            
             outcomesData = response.data
             populateDefaultChart()
         };
@@ -47,12 +42,10 @@
             populateCharts("ALL")
         }
 
-        //this function populates the charts for each category
+        //This uses data from api, takes in the filter type, and builds small multiples charts of each of the filter's categories
         function populateCharts(filterType){
-            console.log("filter type: " + filterType)
             viewModel.chartConfigs = [];
             var subDemographics = outcomesData[filterType]       
-            // var subDemographics = response[filterType]
 
             //This function gets the maximum value to use for all charts in the chosen filter
             function getMaxY(){
@@ -67,113 +60,113 @@
                 }
                 return maxY
             }
+
             var maxY = getMaxY()
 
             // loops through each of the sub-categories within the chosen filter type
             for (subDemographic in subDemographics){
                 var outcomes = subDemographics[subDemographic]
-                
                 var chartConfig = getChartConfig(subDemographic, outcomes)
                 viewModel.chartConfigs.push(chartConfig);
             } //closes FOR loop
 
             
             //Builds config info for a single chart
-                function getChartConfig(subDemographic, outcomes){ 
+            function getChartConfig(subDemographic, outcomes){ 
 
-                    var chartConfig = {
-                            id: "chart-" + subDemographic,
-                            options: {
-                      
-                                chart: {
-                                      type: 'column'
-                                },
+                var chartConfig = {
+                        id: "chart-" + subDemographic,
+                        options: {
+                  
+                            chart: {
+                                  type: 'column'
+                            },
+                            title: {
+                                text: 'Request Outcomes by ' + subDemographic
+                            },
+                              tooltip: {
+                                headerFormat: '<b>{point.x}</b><br/>',
+                                pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                            },
+                            xAxis: {
+                                categories: ['Success', 'Not Placed/Other']
+                            },
+                            yAxis: {
+                                min: 0,
+                                max: maxY, //create a loop to adjust based on max value in data
                                 title: {
-                                    text: 'Request Outcomes by ' + subDemographic
+                                    text: 'Total requests'
                                 },
-                                  tooltip: {
-                                    headerFormat: '<b>{point.x}</b><br/>',
-                                    pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-                                },
-                                xAxis: {
-                                    categories: ['Success', 'Not Placed/Other']
-                                },
-                                yAxis: {
-                                    min: 0,
-                                    max: maxY, //create a loop to adjust based on max value in data
-                                    title: {
-                                        text: 'Total requests'
-                                    },
-                                    stackLabels: {
-                                        enabled: true,
-                                        style: {
-                                            fontWeight: 'bold',
-                                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                                        }
+                                stackLabels: {
+                                    enabled: true,
+                                    style: {
+                                        fontWeight: 'bold',
+                                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
                                     }
-                                },
-                                legend: {
-                                    align: 'right',
-                                    x: -30,
-                                    verticalAlign: 'top',
-                                    y: 25,
-                                    floating: true,
-                                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-                                    borderColor: '#CCC',
-                                    borderWidth: 1,
-                                    shadow: false
-                                },
-                                plotOptions: {
-                                    column: {
-                                        stacking: 'normal',
-                                        dataLabels: {
-                                            enabled: true,
-                                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
-                                            style: {
-                                                textShadow: '0 0 3px black'
-                                            }
+                                }
+                            },
+                            legend: {
+                                align: 'right',
+                                x: -30,
+                                verticalAlign: 'top',
+                                y: 25,
+                                floating: true,
+                                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                                borderColor: '#CCC',
+                                borderWidth: 1,
+                                shadow: false
+                            },
+                            plotOptions: {
+                                column: {
+                                    stacking: 'normal',
+                                    dataLabels: {
+                                        enabled: true,
+                                        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                                        style: {
+                                            textShadow: '0 0 3px black'
                                         }
                                     }
                                 }
-                                
-                              },
-                              
-                          series: [{
-                                name: "Placed",
-                                data: [[0, outcomes["placement"]]]
-                                // data: [outcomes["placement"], outcomes["no placement"] ]
-                            },
-                            {
-                                name: "Referred",
-                                // data: [5, 4]
-                                data: [[0, outcomes["referred"]]]
-                            },
-                             {
-                                name: 'Not placed',
-                                // data: [2, 7]
-                                data: [[1, outcomes["referred"]]]
-                            },
-                            {
-                                name: 'Other',
-                                data: [[1, outcomes["Other"]]]
                             }
-                             ],
                             
-
-                          loading: false,
+                          },
                           
-                          useHighStocks: false,
-                          
-                          
-                          //function (optional)
-                          func: function (chart) {
-                           //setup some logic for the chart
-                          }
+                      series: [{
+                            name: "Placed",
+                            data: [[0, outcomes["placement"]]]
+                            // data: [outcomes["placement"], outcomes["no placement"] ]
+                        },
+                        {
+                            name: "Referred",
+                            // data: [5, 4]
+                            data: [[0, outcomes["referred"]]]
+                        },
+                         {
+                            name: 'Not placed',
+                            // data: [2, 7]
+                            data: [[1, outcomes["referred"]]]
+                        },
+                        {
+                            name: 'Other',
+                            data: [[1, outcomes["Other"]]]
+                        }
+                         ],
                         
-                    };//closes chartConfig object
-                    return chartConfig
-                                      
-                };//closes getChartConfig                            
+
+                      loading: false,
+                      
+                      useHighStocks: false,
+                      
+                      
+                      //function (optional)
+                      func: function (chart) {
+                       //setup some logic for the chart
+                      }
+                    
+                };//closes chartConfig object
+                return chartConfig
+                                  
+            };//closes getChartConfig                            
         }; //closes populateCharts
     }; //closes outcomeController	
 })();//closes document ready
