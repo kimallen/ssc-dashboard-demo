8014a498170dcb66f296c07e413a5a8c1a61e120
kimallen <kimmarieallen@gmail.com>
2016-07-03 18:56:57 -0700

    Move fakedata file to db directory

--- routes/outcomes.js
+++ routes/outcomes.js
@@ -21,11 +21,11 @@ var requestSchema = new db.Schema({
 								 traffickingType: Array,
 								 governmentId: Array,
 								 languages: [{primary: String, secondary: String}]
-							 }], // can alternately be referencing a Need object: need.assessments
+							 }],
 	response: Array, //[{yes: Number, no: Number, noResponse: Number }] or alternately referencing a Response object: response.response
 	timeToResponse: Number,
 	timeToMaybe: Number,
-	outcome: String // options: placed, infoGiven, noPlacement, other
+	outcome: String // options: placed, Info given, No Placement, other
 });
 
 // db.model('Request', requestSchema, 'request')
@@ -71,54 +71,12 @@ var selectedRequestsByDateRegion = db.request.aggregate([
 				{ 
 					$match: {$and: 
 						[
-							{requestDate: {$gte: 1432323613613, $lte: Date.now()}},
-							{region: "SF Bay Area"}
+							{requestDate: {$gte: 1432323613613, $lt: 1440198557701}},
+							{group: "New Jersey"}
 						]
 					}
 				}
 			]);
-//Test:
-db.request.aggregate([{$group: {_id: 'outcome', count: {$sum: 1}}}]);
-//returns { "_id" : "outcome", "count" : 8 }
-	// Get the records within a certain date range from a requested region (or ALL). $match
-	var selectedRequestsByDateRegion = db.requests.aggregate([
-				{ // get all the documents that fall under these dates and region parameters
-					$match: {$and: 
-						[
-							{requestDate: {$gte: startDate, $lte: endDate}},
-							{region: region}
-						]
-					},
-					// use $project to select the fields we need: assessments, outcome
-					$project: {
-										_id: 0,
-										outcome: 1,
-										age: $assessments.age
-										},
-					//returns assessment and outcome data 
-
-					// http://stackoverflow.com/questions/16772156/nested-grouping-with-mongodb
-					$group: {_id: 
-											{age:'$age', outcome: '$outcome'}
-									},
-					$group: {_id: "$_id.age"},
-										outcomes: 
-									}
-
-					// return each age group and the outcome
-				}
-			]);
-
-// Count the result of each type of outcome
-	var superSelectedRequests = selectedRequestsByDateRegion.aggregate([
-			{ 
-					$group: { 
-						"_id": outcome, num_outcome: {$sum: 1}
-					}
-
-				}
-
-		]);
 
 
 	var data = { 
