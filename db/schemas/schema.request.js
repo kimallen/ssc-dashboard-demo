94b3b0022e9fbb7a84f0e98a45a4757f420313be
kimallen <kimmarieallen@gmail.com>
2016-07-19 15:17:02 -0700

    Install dotenv, create .env file with desired constants

---
+++ db/schemas/schema.request.js
@@ -0,0 +1,27 @@
+// var mongoose = require('mongoose');
+
+// var db = mongoose.connect(process.env.APIURL)
+
+// var requestSchema = new db.Schema({
+// 	region: String,
+// 	requestDate: Date,
+// 	age: String, 
+//   substanceAbuse: String,
+//   english: String,
+//   immigrationStatus: String,
+//   mentalIllness: String,
+//   historyOfViolence: Array,
+//   disabilities: Array,
+//   children: Array,
+//   genderId: Array,
+//   traffickingType: Array,
+//   governmentId: Array,
+//   primaryLanguage: String,
+// 	secondaryLanguage: String,
+// 	response: {Maybe: Number, No: Number, "No response": Number},
+// 	timeToFirstMaybe: Number,
+// 	timeToFirstResponse: Number,
+// 	outcome: String
+// });
+
+// module.exports = requestSchema;
