ae5de3bc33d7ab7e626a244688915c1c1f6977a9
kimallen <kimmarieallen@gmail.com>
2016-07-19 17:40:16 -0700

    Create demo mode and variables to display 'Demo data only' when in demo mode

--- public/dashboard/dashboard.config.js
+++ public/dashboard/dashboard.config.js
@@ -1,13 +1,26 @@
-
+// config.demo = true;
+//then put into ??appropriate file??:
 
 angular
         .module('dashboard')
         .config( ['$stateProvider', '$urlRouterProvider', config] )
 
+//change these values for configuration changes
 const OUTCOMEURL = 'http://localhost:3000/api/outcomes';
 const REGIONFILTERS={region1:{value: "New+Jersey", name: "New Jersey"},
                 region2:{value: "SF+Bay+Area", name: "SF Bay Area"},
                 region3:{value: "Texas", name: "Texas"}}
+var demoMode = true;
+
+
+demoState()
+function demoState(){
+     var el = document.getElementById('demo');
+    if (demoMode === true){
+         console.log('visible ' + el)
+         el.style.display='block';
+        }
+};
 
 function config($stateProvider, $urlRouterProvider){
         $urlRouterProvider.otherwise("/home");
