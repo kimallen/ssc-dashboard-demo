a71636db7d01e351f11767f5722d5b4a8fa717af
kimallen <kimmarieallen@gmail.com>
2016-07-20 20:43:28 -0700

    Change http to https for cdn urls in index.html

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users');
+var responses = require('.routes/responses');
 var outcomes = require('./routes/outcomes');
-
 var app = express();
-
+ 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
@@ -28,8 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 // app.use('/users', users);
+app.use('/api/responses', responses);
 app.use('/api/outcomes', outcomes);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- config.js
+++ config.js
@@ -7,10 +7,6 @@ config.db.url = process.env.MONGODB_URI;
 config.regions = process.env.REGIONS.split(",");
 
 config.demo = true;
-//then put into ??appropriate file??:
-// if (config.demo === true){
-// 	var el = document.getElementById('demo');
-// 	el.style.visibility='hidden';
-// }
+
 module.exports = config;
 
